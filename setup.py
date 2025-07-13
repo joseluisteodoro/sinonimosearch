8752dcdfa0c3ae083069d6249b9ba1d4c776bdd1
joseluisteodoro <joseltodoro2@gmail.com>
2025-07-13 19:28:24 -0300

    Versão 0.0.1 buscador de sinonimos

--- setup.py
+++ setup.py
@@ -6,16 +6,15 @@ with open("README.md", "r") as f:
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-setup(
-    name="package_name",
+setup (name="sinonimosearch",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="jose",
+    author_email="joselteodoro2@gmail.com",
+    description="buscador de sinonimo",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/joseluisteodoro/sinonimosearch/tree/master",
     packages=find_packages(),
     install_requires=requirements,
-    python_requires='>=3.8',
-)
+    python_requires='>=3.8')
+    
---
+++ sinonimosearch/buscarsinonimo.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+def buscarporpalavra(palavra):
+    url = f"https://www.sinonimos.com.br/{palavra}"
+    # headers = {"User-Agent": "Mozilla/5.0"}
+    session = requests.Session()
+    resposta = session.get(url)
+
+    if resposta.status_code != 200:
+        return ["Erro ao tentar acessar o site: {resposta.status_code}"]
+
+    sopa = BeautifulSoup(resposta.text,'html.parser')
+
+    bloco = sopa.find('p',class_='syn-list syn-list-1')
+
+    if not bloco:
+        return[f"Nenhum sinonimo encontrado para palavra {palavra}"]
+    
+    sinonimos_tags = bloco.find_all('a', class_='sinonimo')
+    sinonimos = [tag.text.strip() for tag in sinonimos_tags]
+
+    print(f"Sinônimos encontrados para: {palavra}")
+    print("-"*40)
+    for s in sinonimos:
+        print(f"\t• {s}")
+    print("-"*40 + f"\n Total encontrados: {len(sinonimos)}\n")
+
+buscarporpalavra("tornado")
+    
